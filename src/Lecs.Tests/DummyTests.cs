1bdafbff47708d0d6aabf4a7aaece19051eef5d3
Bastian Blokland <bastianblokland@gmail.com>
2018-12-19 13:06:24 +0100

    Add basic 'Lecs' and 'Lecs.Tests' projects

---
+++ src/Lecs.Tests/DummyTests.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Lecs.Tests
+{
+    public class DummyTests
+    {
+        [Fact]
+        public void Dummy_ReplaceMeWithRealTest()
+        {
+
+        }
+    }
+}
---
+++ src/Lecs/DummyClass.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Lecs
+{
+    public class DummyClass
+    {
+    }
+}
