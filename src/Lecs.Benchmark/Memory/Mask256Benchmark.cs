6ef990af876e2950d13d25ea9451af17389a2549
Bastian Blokland <bastianblokland@gmail.com>
2018-12-27 11:23:26 +0100

    Add additional benchmarks for 'Mask256'

--- src/Lecs.Benchmark/Memory/Mask256Benchmark.cs
+++ src/Lecs.Benchmark/Memory/Mask256Benchmark.cs
@@ -5,55 +5,6 @@ using Lecs.Memory;
 
 namespace Lecs.Benchmark.Memory
 {
-    [MemoryDiagnoser]
-    public class Mask256_AddRemove_Benchmark : Mask256_BaseBenchmark
-    {
-        [Benchmark(Baseline = true)]
-        public void AddRemove_ReferenceMask64()
-        {
-            var toAdd = referenceMasks[0];
-            for (int i = 1; i < referenceMasks.Length; i++)
-            {
-                referenceMasks[i].Add(toAdd);
-                referenceMasks[i].Remove(toAdd);
-            }
-        }
-
-        [Benchmark]
-        public void AddRemove_SoftwareMask256()
-        {
-            var toAdd = masks[0];
-            for (int i = 1; i < masks.Length; i++)
-            {
-                masks[i].AddSoftware(in toAdd);
-                masks[i].RemoveSoftware(in toAdd);
-            }
-        }
-
-        [Benchmark]
-        public void AddRemove_AvxMask256()
-        {
-            var toAdd = masks[0];
-            for (int i = 1; i < masks.Length; i++)
-            {
-                masks[i].AddAvx(in toAdd);
-                masks[i].RemoveAvx(in toAdd);
-            }
-        }
-
-        [Benchmark]
-        public void AddRemove_Avx2Mask256()
-        {
-            var toAdd = masks[0];
-            for (int i = 1; i < masks.Length; i++)
-            {
-                masks[i].AddAvx2(in toAdd);
-                masks[i].RemoveAvx2(in toAdd);
-            }
-        }
-    }
-
-    [MemoryDiagnoser]
     public class Mask256_HasAll_Benchmark : Mask256_BaseBenchmark
     {
         [Benchmark(Baseline = true)]
@@ -87,7 +38,6 @@ namespace Lecs.Benchmark.Memory
         }
     }
 
-    [MemoryDiagnoser]
     public class Mask256_HasAny_Benchmark : Mask256_BaseBenchmark
     {
         [Benchmark(Baseline = true)]
@@ -121,7 +71,6 @@ namespace Lecs.Benchmark.Memory
         }
     }
 
-    [MemoryDiagnoser]
     public class Mask256_NotHasAny_Benchmark : Mask256_BaseBenchmark
     {
         [Benchmark(Baseline = true)]
@@ -155,9 +104,208 @@ namespace Lecs.Benchmark.Memory
         }
     }
 
+    public class Mask256_Add_Benchmark : Mask256_BaseBenchmark
+    {
+        /*
+        Note: These benchmarks have the side-effect of actually changing the data, which is bad but
+        in this case i know that none of the implementations benefit from a specific input-data
+        and making copies affects the results too much.
+        */
+
+        [Benchmark(Baseline = true)]
+        public void Add_ReferenceMask64()
+        {
+            var toAdd = referenceMasks[0];
+            for (int i = 1; i < referenceMasks.Length; i++)
+                referenceMasks[i].Add(toAdd);
+        }
+
+        [Benchmark]
+        public void Add_SoftwareMask256()
+        {
+            var toAdd = masks[0];
+            for (int i = 1; i < masks.Length; i++)
+                masks[i].AddSoftware(in toAdd);
+        }
+
+        [Benchmark]
+        public void Add_AvxMask256()
+        {
+            var toAdd = masks[0];
+            for (int i = 1; i < masks.Length; i++)
+                masks[i].AddAvx(in toAdd);
+        }
+
+        [Benchmark]
+        public void Add_Avx2Mask256()
+        {
+            var toAdd = masks[0];
+            for (int i = 1; i < masks.Length; i++)
+                masks[i].AddAvx2(in toAdd);
+        }
+    }
+
+    public class Mask256_Remove_Benchmark : Mask256_BaseBenchmark
+    {
+        /*
+        Note: These benchmarks have the side-effect of actually changing the data, which is bad but
+        in this case i know that none of the implementations benefit from a specific input-data
+        and making copies affects the results too much.
+        */
+
+        [Benchmark(Baseline = true)]
+        public void Remove_ReferenceMask64()
+        {
+            var toAdd = referenceMasks[0];
+            for (int i = 1; i < referenceMasks.Length; i++)
+                referenceMasks[i].Remove(toAdd);
+        }
+
+        [Benchmark]
+        public void Remove_SoftwareMask256()
+        {
+            var toAdd = masks[0];
+            for (int i = 1; i < masks.Length; i++)
+                masks[i].RemoveSoftware(in toAdd);
+        }
+
+        [Benchmark]
+        public void Remove_AvxMask256()
+        {
+            var toAdd = masks[0];
+            for (int i = 1; i < masks.Length; i++)
+                masks[i].RemoveAvx(in toAdd);
+        }
+
+        [Benchmark]
+        public void Remove_Avx2Mask256()
+        {
+            var toAdd = masks[0];
+            for (int i = 1; i < masks.Length; i++)
+                masks[i].RemoveAvx2(in toAdd);
+        }
+    }
+
+    public class Mask256_Invert_Benchmark : Mask256_BaseBenchmark
+    {
+        /*
+        Note: These benchmarks have the side-effect of actually changing the data, which is bad but
+        in this case i know that none of the implementations benefit from a specific input-data
+        and making copies affects the results too much.
+        */
+
+        [Benchmark(Baseline = true)]
+        public void Invert_ReferenceMask64()
+        {
+            var toAdd = referenceMasks[0];
+            for (int i = 1; i < referenceMasks.Length; i++)
+                masks[i].Invert();
+        }
+
+        [Benchmark]
+        public void Invert_SoftwareMask256()
+        {
+            var toAdd = masks[0];
+            for (int i = 1; i < masks.Length; i++)
+                masks[i].InvertSoftware();
+        }
+
+        [Benchmark]
+        public void Invert_AvxMask256()
+        {
+            var toAdd = masks[0];
+            for (int i = 1; i < masks.Length; i++)
+                masks[i].InvertAvx();
+        }
+
+        [Benchmark]
+        public void Invert_Avx2Mask256()
+        {
+            var toAdd = masks[0];
+            for (int i = 1; i < masks.Length; i++)
+                masks[i].InvertAvx2();
+        }
+    }
+
+    public class Mask256_Clear_Benchmark : Mask256_BaseBenchmark
+    {
+        /*
+        Note: These benchmarks have the side-effect of actually changing the data, which is bad but
+        in this case i know that none of the implementations benefit from the input-data being clear
+        and making copies affects the results too much.
+        */
+
+        [Benchmark(Baseline = true)]
+        public void Clear_ReferenceMask64()
+        {
+            var toAdd = referenceMasks[0];
+            for (int i = 1; i < referenceMasks.Length; i++)
+                masks[i].Clear();
+        }
+
+        [Benchmark]
+        public void Clear_SoftwareMask256()
+        {
+            var toAdd = masks[0];
+            for (int i = 1; i < masks.Length; i++)
+                masks[i].ClearSoftware();
+        }
+
+        [Benchmark]
+        public void Clear_Avx2Mask256()
+        {
+            var toAdd = masks[0];
+            for (int i = 1; i < masks.Length; i++)
+                masks[i].ClearAvx2();
+        }
+    }
+
+    public class Mask256_Equals_Benchmark : Mask256_BaseBenchmark
+    {
+        [Benchmark(Baseline = true)]
+        public bool Equals_ReferenceMask64()
+        {
+            var toCheck = referenceMasks[0];
+            var result = true;
+            for (int i = 1; i < referenceMasks.Length; i++)
+                result &= referenceMasks[i].Equals(toCheck);
+            return result;
+        }
+
+        [Benchmark]
+        public bool Equals_SoftwareMask256()
+        {
+            var toCheck = masks[0];
+            var result = true;
+            for (int i = 1; i < masks.Length; i++)
+                result &= masks[i].EqualsSoftware(in toCheck);
+            return result;
+        }
+
+        [Benchmark]
+        public bool Equals_AvxMask256()
+        {
+            var toCheck = masks[0];
+            var result = true;
+            for (int i = 1; i < masks.Length; i++)
+                result &= masks[i].EqualsAvx(in toCheck);
+            return result;
+        }
+
+        [Benchmark]
+        public bool Equals_Avx2Mask256()
+        {
+            var toCheck = masks[0];
+            var result = true;
+            for (int i = 1; i < masks.Length; i++)
+                result &= masks[i].EqualsAvx2(in toCheck);
+            return result;
+        }
+    }
+
     public abstract class Mask256_BaseBenchmark
     {
-        protected const int ElementCount = 1000;
+        protected const int ElementCount = 10_000;
 
         protected readonly ReferenceMask64[] referenceMasks = new ReferenceMask64[ElementCount];
         protected readonly Mask256[] masks = new Mask256[ElementCount];
@@ -217,5 +365,7 @@ namespace Lecs.Benchmark.Memory
         public void Invert() => data = ~data;
 
         public void Clear() => data = 0;
+
+        public bool Equals(ReferenceMask64 other) => data == other.data;
     }
 }
