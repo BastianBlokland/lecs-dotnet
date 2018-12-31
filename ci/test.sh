bf0f02bf9571a1a7b109cff8468cbda7142616f3
Bastian Blokland <bastianblokland@gmail.com>
2018-12-31 18:40:39 +0100

    Add minimum test coverage threshold of 75%

--- ci/test.sh
+++ ci/test.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 set -e
 
+COVERAGE_THRESHOLD=75
 TEST_RESULT_PATH="./../../artifacts/xunit.results.xml"
 COVERAGE_RESULT_PATH="./../../artifacts/coverage.cobertura.xml"
 REPORT_RESULT_PATH="./../../artifacts/coverage_report"
@@ -8,7 +9,7 @@ REPORT_RESULT_PATH="./../../artifacts/coverage_report"
 # Run test
 dotnet test src/Lecs.Tests/Lecs.Tests.csproj \
     --logger "xunit;LogFilePath=$TEST_RESULT_PATH" \
-    /p:CollectCoverage=true /p:Include="[Lecs]*" /p:UseSourceLink=true \
+    /p:CollectCoverage=true /p:Include="[Lecs]*" /p:UseSourceLink=true /p:Threshold=$COVERAGE_THRESHOLD \
     /p:CoverletOutputFormat=cobertura /p:CoverletOutput=$COVERAGE_RESULT_PATH
 
 # Create coverage report
